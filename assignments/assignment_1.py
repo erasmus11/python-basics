c2cd5018475efe528a7af8a2af1256a59b3ffbe7
erasmus11 <erasmuschawey12345@gmail.com>
2023-03-16 17:51:19 +0000

    my first python assignment

---
+++ assignments/assignment_1.py
@@ -0,0 +1,18 @@
+#Write a python script that takes input from a user and displays it to the user in a well fromatted output.(use concept of list) userDetails:name,age,class,gender,location/address
+userDetails=[]
+userInput=input("Enter a name: ")
+userDetails.append(userInput)
+userInput=input("Enter your age:")
+userDetails.append(userInput)
+uerInput=input("Enter your class:")
+userDetails.append(userInput)
+userInput=input("Enter your gender:")
+userDetails.append(userInput)
+userInput=input("Enter your phonenumber: ")
+userDetails.append(userInput)                                               
+#print(userInput)
+#print(userDetails)
+
+
+
+
